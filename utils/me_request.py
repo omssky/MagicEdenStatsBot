96c95bf670749da914dd24a1dd5b1bf1f40dad32
omssky.dev <omssky.dev@gmail.com>
2023-08-19 14:02:09 +0300

    maint upd

--- utils/me_request.py
+++ utils/me_request.py
@@ -3,7 +3,7 @@ import logging
 
 # This fuction formats json responce into text message
 async def format_info(info_json) -> str:
-    info = (f"Name: <b>{info_json['name']}</b> 📊\n"
+    info = (f"Name: <b>{info_json['symbol']}</b> 📊\n"
             f"Floor price: {info_json.get('floorPrice', 0)/10**9} ◎\n"
             f"Listings: {info_json.get('listedCount', 0)}\n"
             f"Avg. sale (24h): {round(info_json.get('avgPrice24hr', 0)/10**9, 3)} ◎\n"
@@ -15,7 +15,7 @@ async def collecion_info(user_query):
     async with aiohttp.ClientSession() as session:
         for sep in ['_', '']:
             collection_symbol=user_query.replace(' ', sep)
-            response = await session.get(f'https://api-mainnet.magiceden.dev/v2/collections/{collection_symbol}')
+            response = await session.get(f'https://api-mainnet.magiceden.dev/v2/collections/{collection_symbol}/stats')
             response_json = await response.json()
             if response.status != 404:
                 logging.info(f"Search [{user_query}:{collection_symbol}] SUCCSESS")
